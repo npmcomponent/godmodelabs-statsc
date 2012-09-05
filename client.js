dff0694bc84f2be775617fffe239bcbd4b9fafe8
Julian Gruber <julian@juliangruber.com>
2012-09-05 12:18:49 +0200

    server implemented

--- client.js
+++ client.js
@@ -1,54 +1,58 @@
 ;(function() {
 
   var statsc = {};
-  var addr;
+  var addr, tag;
 
   var queue = [];
 
   statsc.connect = function(_addr) {
+    tag = document.createElement('script');
+    document.getElementsByTagName('head')[0].appendChild(tag);
     if (_addr) return addr = _addr;
-    addr = 'http://localhost:8126/'
+    addr = 'http://localhost:8126/';
   };
 
-  statsc.increment = function(counter) {
-    queue.push({op:'increment', val:counter});
+  statsc.increment = function(counter, sampleRate) {
+    queue.push(['increment', counter, sampleRate]);
   };
 
-  statsc.decrement = function(counter) {
-    queue.push({op:'decrement', val:counter});
+  statsc.decrement = function(counter, sampleRate) {
+    queue.push(['decrement', counter, sampleRate]);
   };
 
-  statsc.gauge = function(gauge, value) {
-    queue.push({op:'gauge', val:value});
+  statsc.gauge = function(gauge, value, sampleRate) {
+    queue.push(['gauge', value, sampleRate]);
   };
 
   function fromNow(time) {
     return new Date().getTime() - time;
   }
 
-  statsc.timing = function(timing, time) {
-    if ('number' == typeof time) return queue.push({op:'timing', val:time});
-    if (time instanceof Date) return queue.push({op:'timing', val:fromNow(time.getTime())})
+  statsc.timing = function(timing, time, sampleRate) {
+    if ('number' == typeof time) return queue.push(['timing', time, sampleRate]);
+    if (time instanceof Date) return queue.push(['timing', fromNow(time.getTime()), sampleRate]);
     if ('function' == typeof time) {
       var start = new Date().getTime();
       time();
-      return queue.push({op:'timing', val:fromNow(start)});
+      return queue.push(['timing', fromNow(start), sampleRate]);
     }
   };
 
-  statsc.timer = function(timing) {
+  statsc.timer = function(timing, sampleRate) {
     var start = new Date().getTime();
 
     return function() {
-      queue.push({op:'timing', val:fromNow(start)});
+      queue.push(['timing', fromNow(start), sampleRate]);
     }
   };
 
   setInterval(function() {
     if (queue.length > 0) {
-      $.post(addr, JSON.stringify(queue), function() {
-        queue = [];
-      }).error(function() { /* no-op */ });
+      // $.post(addr, JSON.stringify(queue), function(data) {
+      //         queue = [];
+      //         console.log(data);
+      //       }).error(function() { /* no-op */ });
+      tag.src = addr+JSON.stringify(queue);
     }
   }, 5000);
 
--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 var http = require('http');
-statsd = new require('node-statsd')('test02', 8125);
+var url = require('url');
+var log = new require('statistik')();
+var validMethods = ['increment', 'decrement', 'timing', 'gauge', 'send'];
 
 var server = http.createServer(function(req, res) {
-	if (req.method == 'POST') {
-		var data = '';
-		req.on('data', function(chunk) {
-			data += chunk.toString();
-			if (data.length > 1e6) req.connection.destroy();
-		});
-		req.on('end', function() {
-			res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-			res.end();
-		});
-	} else {
-		res.end();
-	}
+  res.writeHead(200, {'Content-Type': 'application/javascript'});
+	try {
+	  if (req.method != 'GET') throw Error('Not supported');
+	  var url = decodeURIComponent(req.url).replace(/\//g, '');
+	  if (url == 'favicon.ico') return res.end();
+	  if (url[0] != '[') throw Error('File serving not supported');
+    var ops = JSON.parse(url);
+    var op;
+    for (var i = 0, len = ops.length; i < len; i++) {
+      op = ops[i];
+      if (!isArray(op)) throw Error('INVALID');
+      if (validMethods.indexOf(op[0]) == -1) {
+        throw Error('Method `'+op[0]+'` not supported');
+      }
+      log[op[0]].apply(log, op.splice(1));
+    }
+		res.end('"OK";');
+  } catch(e) {
+    res.end('"'+e.toString()+'";');
+    console.error(e);
+  }
 });
+
+function isArray(o) {
+  return typeof o == 'object' && o instanceof Array;
+}
 
 server.listen(8126);
